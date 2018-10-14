3219e1271a854812cacabe5d3426ac0b80f74e31
Marcus Higgins <halo2305@gmail.com>
2018-10-14 14:23:27 -0500

    changes to driver.java

--- Robotics2/src/Driver.java
+++ Robotics2/src/Driver.java
@@ -1,63 +1,52 @@
-import java.util.ArrayList;
+import java.io.DataOutputStream;
 
+import lejos.hardware.Bluetooth;
 import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.MindsensorsAbsoluteIMU;
 import lejos.hardware.sensor.SensorMode;
+import lejos.remote.nxt.NXTCommConnector;
+import lejos.remote.nxt.NXTConnection;
 
 public class Driver {
 	
-	public static Port port = LocalEV3.get().getPort("S1");
-	public static MindsensorsAbsoluteIMU gyro = new MindsensorsAbsoluteIMU(port);
-	public static SensorMode gyroprovider = (SensorMode) gyro.getAccelerationMode();
-	public static float[] sample = new float[gyro.sampleSize()];
+
 	
 	public static final int threshold = 5;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		switchMode();
-
-	}
-	
-	public static void switchMode() {
+		Port port = LocalEV3.get().getPort("S1");
+		MindsensorsAbsoluteIMU gyro = new MindsensorsAbsoluteIMU(port);
+		SensorMode gyroprovider = (SensorMode) gyro.getAccelerationMode();
+		float[] sample = new float[gyro.sampleSize()];
+		
+		NXTCommConnector connector = Bluetooth.getNXTCommConnector();
+		NXTConnection connection = connector.connect("00:16:53:44:66:05", NXTConnection.RAW);
+		DataOutputStream output = connection.openDataOutputStream();
+		
+		byte[] data = new byte[1];
 		
 		while(Button.getButtons() != Button.ID_ESCAPE) {
 		gyroprovider.fetchSample(sample, 0);
 		
 		if(sample[0] < -threshold)
-			forward();
+			data[0] = 0;
 		else if (sample[0] > threshold)
-			back();
+			data[0] = 1;
 		else if (sample[1] > threshold)
-			left();
+			data[0] = 2;
 		else if (sample[1] < -threshold)
-			right();
+			data[0] = 3;
+		
+		output.write(data);
 		
 		}
 		
-	}
-	
-	public static void forward() {
-		LCD.clearDisplay();
-		LCD.drawString("forward", 0, 0);
-	}
-	
-	public static void back() {
-		LCD.clearDisplay();
-		LCD.drawString("back", 0, 0);
-	}
-	
-	public static void left() {
-		LCD.clearDisplay();
-		LCD.drawString("left", 0, 0);
-	}
-	
-	public static void right() {
-		LCD.clearDisplay();
-		LCD.drawString("right", 0, 0);
+		gyro.close();
+
 	}
 
 }
