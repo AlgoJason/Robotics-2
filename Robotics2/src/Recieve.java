b24146154ba27a68c37ea461717f3a46f6b60931
JDINH6 <JasonDinh@outlook.com>
2018-10-14 14:25:22 -0500

    Recieve

--- Robotics2/src/Recieve.java
+++ Robotics2/src/Recieve.java
@@ -6,35 +6,55 @@ import lejos.remote.nxt.NXTCommConnector;
 import lejos.remote.nxt.NXTConnection;
 import lejos.utility.Delay;
 
-
 public class Recieve {
+	// Global Var
+	public static NXTCommConnector connector = Bluetooth.getNXTCommConnector();
+	public static NXTConnection connect = connector.waitForConnection(0, NXTConnection.RAW);
+	public static DataInputStream input = connect.openDataInputStream();
+	public static DataOutputStream output = connect.openDataOutputStream();
+	public static byte[] n = new byte[0];
+
+
+	public static void forward() {
+		if (n[0] == 0) {
+
+		}
+	}//end forward
+
+
+
+	public static void start() throws Exception {
+		NXTCommConnector connector = Bluetooth.getNXTCommConnector();
+		System.out.println("Waiting for connection ...");
+		NXTConnection connect = connector.waitForConnection(0, NXTConnection.RAW);
+		System.out.println("Connected");
+		DataInputStream input = connect.openDataInputStream();
+		DataOutputStream output = connect.openDataOutputStream();
+
+		while (true) {
+			try {
+				if (input.read(n) == -1)
+					break;
+			} catch (EOFException e) {
+				break;
+			}
+			System.out.println("Read " + n[0]);
+		} // end while
+	}// end start
+
+	public static void endprogram() throws Exception {
+		Delay.msDelay(1000);
+		input.close();
+		output.close();
+		connect.close();
+	}// end endprgram
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		NXTCommConnector connector = Bluetooth.getNXTCommConnector();
-		System.out.println("Waiting for connection ...");
-		NXTConnection con = connector.waitForConnection(0, NXTConnection.RAW);
-		System.out.println("Connected");
-		DataInputStream dis = con.openDataInputStream();
-        DataOutputStream dos = con.openDataOutputStream();
+		start();
+		endprogram();
 
 
-        byte[] n = new byte[8]; while(true)
-        {
-        try{
-        if (dis.read(n) == -1) break;
-        } catch (EOFException e)
-        {
-        break;
-        }
-        System.out.println("Read " + n[0]+n[1]+ n[2]+n[3]+ n[4]+n[5]+ n[6]+n[7]);
-                    dos.write(n);
-                    dos.flush();
-                            }
-                    Delay.msDelay(1000);
-                            dis.close();
-                            dos.close();
-                            con.close();
-	}//end main
+	}// end main
 
-}//end class
+}// end class
