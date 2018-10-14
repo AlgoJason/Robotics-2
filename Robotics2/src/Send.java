60bfcb348d90c583917765acc097411fa6793304
JDINH6 <JasonDinh@outlook.com>
2018-10-14 12:56:19 -0500

    Orginal

--- Robotics2/src/Send.java
+++ Robotics2/src/Send.java
@@ -1,130 +1,47 @@
-import java.io.DataOutputStream;
 import java.io.DataInputStream;
-
+import java.io.DataOutputStream;
 import lejos.hardware.Bluetooth;
 import lejos.hardware.Button;
 import lejos.remote.nxt.NXTCommConnector;
 import lejos.remote.nxt.NXTConnection;
+import lejos.utility.Delay;
 
 public class Send {
-	//port declared
-	public static Port mindSensor = LocalEV3.get().getPort("S1");
-	//sensor
-	public static MindsensorsAbsoluteIMU info = new MindsensorsAbsoluteIMU(mindSensor);
-	//Connection setup [values assigned in initialize()]
-
-	public static SensorValue(){
-		SensorMode distancefinder = (SensorMode) info.getAngleMode();
-		float[] test = new float[info.sampleSize()];
-		LCD.drawString("Distance = " + test[0], 0, 0);
-		Delay.msDelay(100);
-	}
+	private static final String EV3 = "00:16:53:44:66:05"; //Chen1
 
 
 	public static void main(String[] args) throws Exception {
-		SensorValue();
 		NXTCommConnector connector = Bluetooth.getNXTCommConnector();
-		NXTConnection connection = connector.connect("00:16:53:44:66:05", NXTConnection.RAW);
-<<<<<<< HEAD
+		System.out.println("Connecting to " + EV3);
+		NXTConnection connection = connector.connect(EV3, NXTConnection.RAW);
+		System.out.println("Connected");
 
-		connector.waitForConnection(0, NXTConnection.RAW);
+		 DataInputStream input =
+				 connection.openDataInputStream();
+				         DataOutputStream output =
+				 connection.openDataOutputStream();
 
-		DataOutputStream output = connection.openDataOutputStream();
-		DataInputStream input = connection.openDataInputStream();
+				        System.out.println("Sending Data");
 
-		byte[] nums = new byte[1];
-		nums[0] = 15;
+				        int cnt = 0;
+				        byte[] o = new byte[8];
+				        byte[] r = new byte[8];
 
-		output.write(nums);
-		output.flush();
+				        while (Button.getButtons() != Button.ID_ESCAPE) {
+				        	Button.waitForAnyPress();
+				        	for(int i = 0; i < 8; i++)
+				        		o[i] = (byte) cnt++;
+				        	output.write(o);
+				        	 output.flush();
+				        	 input.read(r);
 
-		//comm(output);
-
-=======
-		
-		connector.waitForConnection(0, NXTConnection.RAW);
-		
-		connector.waitForConnection(0, NXTConnection.RAW);
-		
-		DataOutputStream output = connection.openDataOutputStream();
-		DataInputStream input = connection.openDataInputStream();
-		
-		byte[] nums = new byte[1];
-		nums[0] = 15;
-<<<<<<< HEAD
-		
-		output.write(nums);
-		output.flush();
-=======
-		
-		output.write(nums);
-		output.flush();
-		
-		//comm(output);
-		
-		Button.waitForAnyPress();
->>>>>>> parent of 430e996... put stuff back into comm()
-		
-		//comm(output);
-		
->>>>>>> parent of 430e996... put stuff back into comm()
-		Button.waitForAnyPress();
-
-		output.close();
-		connection.close();
-	}
-
-	//demo code from class
-	/*
-	public static void demo(DataOutputStream output) throws Exception {
-		System.out.println("Sending data");
-
-		int cnt = 0;
-		byte[] o = new byte[8];
-		byte[] r = new byte[8];
-
-		while (Button.getButtons() != Button.ID_ESCAPE) {
-			Button.waitForAnyPress();
-			for (int i = 0; i < 8; i++)
-				o[i] = (byte) cnt++;
-			output.write(o);
-			output.flush();
-
-			System.out.println("Read: " + r[0] + r[1] + r[2] + r[3] + r[4] + r[5] + r[6] + r[7]);
-		}
-		System.out.println("All data sent");
-	}
-
-	//function to test output passing
-
-	public static void comm(DataOutputStream output) throws Exception {
-<<<<<<< HEAD
-
-		System.out.println("before output");
-		while(Button.getButtons() != Button.ID_ESCAPE);
-		//output = connection.openDataOutputStream();
-
-		byte[] nums = new byte[1];
-		nums[0] = 15;
-
-=======
-		
-		System.out.println("before output");
-		while(Button.getButtons() != Button.ID_ESCAPE);
-		//output = connection.openDataOutputStream();
-		
-		System.out.println("before output");
-		while(Button.getButtons() != Button.ID_ESCAPE);
-		//output = connection.openDataOutputStream();
-		
-		byte[] nums = new byte[1];
-		nums[0] = 15;
-		
->>>>>>> parent of 430e996... put stuff back into comm()
-		output.write(nums);
-		output.flush();
-
-	}
-	*/
-
-}
+				        	 System.out.println("Read: " + r[0]+r[1]+r[2]+r[3]+r[4]+r[5]+r[6]+r[7]);
+				        }
+				        System.out.println("All data sent");
+				        Delay.msDelay(1000);
+				        output.close();
+				        input.close();
+				        connection.close();
+				System.out.println("Connection closed");
+				        }//end main
+	}//end class
